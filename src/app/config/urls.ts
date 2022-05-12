50a76f8b829117146a4382baa3a311d12e03d704
Serhii-Bondarenko <sergeybondd@gmail.com>
2022-05-12 19:21:03 +0300

    lesson 1 homework

---
+++ src/app/config/urls.ts
@@ -0,0 +1,9 @@
+const baseURL = 'https://jsonplaceholder.typicode.com';
+
+export const urls = {
+  users: `${baseURL}/users`,
+  posts: `${baseURL}/posts`,
+  comments: `${baseURL}/comments`
+}
+
+export default baseURL;
